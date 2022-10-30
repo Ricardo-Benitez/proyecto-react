f3dac8b442749326a88b86afce1074311ff73369
ricardo benitez <ricardocdelu@gmail.com>
2022-10-30 17:01:32 -0300

    ..

--- proyecto-benitez/src/components/content/DetalleProducto.jsx
+++ proyecto-benitez/src/components/content/DetalleProducto.jsx
@@ -4,11 +4,11 @@ const DetalleProducto = ({producto}) => {
     return (
         <>
          
-  <div className="">
-    <div className="">
+  <div className="row g-0">
+    <div className="col-md-4">
       <img src={`../img/${producto.img}`} className="img-fluid rounded-start" alt="..." />
     </div>
-    <div className="">
+    <div className="col-md-8">
       <div className="card-body">
         <h5 className="card-title">{producto.nombre}</h5>  
         <p className="card-text">{producto.tipo}</p>
